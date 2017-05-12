f87f764a8ca9915aae687610250f98ba9fc780bb
qiansr <caoyiqiansr@gmail.com>
2017-05-12 16:25:51 +0800

    清理文件

---
+++ screen/run.php
@@ -0,0 +1,17 @@
+﻿<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/crawler.php';
+
+use Screen\Crawler;
+
+while (1) {
+    $time = date('Y-m-d H:i:s');
+    echo "info: $time Start...\n";
+    $crawler = new Crawler();
+    $crawler->crawl();
+    unset($crawler);
+    $time = date('Y-m-d H:i:s');
+    echo "info: $time Finish.\n\n";
+    sleep(3600);
+}
